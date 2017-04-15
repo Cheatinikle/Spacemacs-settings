644213fdd4dfe38a543932ebcbcd7b830f34285f
Cheatinikle <kunwoo3526@gmail.com>
2017-04-16 01:36:33 +0900

    Created Temp-Repl package.

---
+++ emacs-package/temp-repl.el
@@ -0,0 +1,52 @@
+(defvar temp-repl--repl-lists (make-hash-table :test 'equal))
+(defvar temp-repl--rest-lists (make-hash-table :test 'equal))
+
+(defun temp-repl-in (htable)
+  (-each (hash-table-keys htable)
+    (lambda (key)
+      (save-excursion
+        (goto-char (point-min))
+        (while (search-forward key nil t)
+          (replace-match (gethash key htable))))))
+  )
+
+(defun temp-repl-apply ()
+  (interactive)
+  (temp-repl-in temp-repl--repl-lists))
+
+(defun temp-repl-restore ()
+  (interactive)
+  (temp-repl-in temp-repl--rest-lists))
+
+(defun temp-repl-add ()
+  (interactive)
+  (let ((from (read-from-minibuffer "From... "))
+        (to (read-from-minibuffer "To... ")))
+    (puthash from to temp-repl--repl-lists)
+    (puthash to from temp-repl--rest-lists)))
+
+(defun temp-repl-remove (target)
+  (interactive "sWhat to remove?")
+  (remhash (gethash target temp-repl--repl-lists) temp-repl--rest-lists)
+  (remhash target temp-repl--repl-lists))
+
+(defun temp-repl-remove-helm ()
+  (interactive)
+  (let ((source `((name . "Which to remove? ")
+                  (candidates . ,(hash-table-keys temp-repl--repl-lists))
+                  (action . (lambda (item) (temp-repl-remove item))))))
+    (helm :sources '(source))))
+
+(defun temp-repl-list ()
+  (interactive)
+  (let ((source `((name . "Temporary Replacing Lists")
+                  (candidates . ,(-zip-with
+                                  (lambda (a b) (concat a " -> " b))
+                                  (hash-table-keys temp-repl--repl-lists)
+                                  (hash-table-values temp-repl--repl-lists))))))
+    (helm :sources '(source))))
+
+(add-hook 'kill-buffer-hook 'temp-repl-restore)
+
+(provide 'temp-repl)
+
