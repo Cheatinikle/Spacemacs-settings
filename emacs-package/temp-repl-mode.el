a54ac9e87c3a5a229c15385a613a5f4a53541000
Cheatinikle <kunwoo3526@gmail.com>
2017-05-02 15:35:42 +0900

    Init.

---
+++ emacs-package/temp-repl-mode.el
@@ -0,0 +1,15 @@
+(require 'temp-repl)
+
+(define-minor-mode temp-repl-mode
+  "Temporary replacement"
+  :init-value nil
+  (if temp-repl-mode
+    (temp-repl-apply)
+    (add-hook 'post-command-hook 'temp-repl-apply)
+    (progn
+      (remove-hook 'post-command-hook 'temp-repl-apply)
+      (temp-repl-restore)))
+  )
+
+(provide 'temp-repl-mode)
+
