b2e8525e53fa2ab354399e74454d3ad53dddb05c
Cheatinikle <kunwoo3526@gmail.com>
2017-05-27 14:53:29 +0900

    Added prefix jsch/

--- emacs-package/just-search.el
+++ emacs-package/just-search.el
@@ -2,75 +2,75 @@
 
 (require 'just-utils)
 
-(defun search-with (prefix keyword)
+(defun jsch/search-with (prefix keyword)
   "Searches keyword with given prefix.
 
    PREFIX : Url prefix. (such as \"http://google.co.kr/#q=\")
    KEYWORD : keyword you want to search.
 
-   ex : (search-with \"http://google.co.kr/#q=\" \"lisp\")"
+   ex : (jsch/search-with \"http://google.co.kr/#q=\" \"lisp\")"
 
   (browse-url (concat prefix keyword)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;;;
 
-(defvar engines (make-hash-table :test 'equal) "Engine lists")
+(defvar jsch/engines (make-hash-table :test 'equal) "Engine dictionary")
 
-(cl-defmacro create-search-command (name &key input engine)
+(cl-defmacro jsch/create-search-command (name &key input engine)
   "A macro to create a keyword-searching function.
    NAME : name of the function you want to create.
    INPUT : function with no argument that returns string.
    ENGINE : function with no argument that returns string.
             (this string must be the name of predefined just-search engines)
 
-   ex : (create-search-command test :input (lambda () \"lisp\")
-                                    :engine (lambda () \"google\"))
+   ex : (jsch/create-search-command test :input (lambda () \"lisp\")
+                                         :engine (lambda () \"google\"))
         => just-search-test"
 
   `(defun ,(macro-conc 'just-search- name) ()
      (interactive)
-     (let ((prefix (get-engine (funcall ,engine))))
+     (let ((prefix (jsch/get-engine (funcall ,engine))))
        (if prefix
-           (search-with prefix (funcall ,input))
-         (message "Invalid engine!")))
+           (jsch/search-with prefix (funcall ,input))
+           (message "Invalid engine!")))
      ))
 
-(defmacro make-engine (name prefix)
+(defmacro jsch/make-engine (name prefix)
   "Creates just-search engine. More exactly, creates a function that automatically searches given keyword and puts it to the just-search engine dictionary.
 
    NAME : name of the engine.
    PREFIX : Url prefix. (such as \"http://google.co.kr/#q=\")
 
-   ex : (make-engine google \"http://google.co.kr/#q=\")"
+   ex : (jsch/make-engine google \"http://google.co.kr/#q=\")"
 
   `(progn
-     (create-search-command ,name :input 'keyword-from-anywhere
-                                  :engine (lambda () ,(symbol-name name)))
-     (puthash ,(symbol-name name) ,prefix engines)))
+     (jsch/create-search-command ,name :input 'jsch/keyword-from-anywhere
+                                       :engine (lambda () ,(symbol-name name)))
+     (puthash ,(symbol-name name) ,prefix jsch/engines)))
 
-(defun get-engine (engine)
+(defun jsch/get-engine (engine)
   "Gets engine from just-search engine dictionary.
 
    ENGINE : name of the engine (must be string type)
 
-   ex : (get-engine \"google\") "
+   ex : (jsch/get-engine \"google\") "
 
-  (gethash engine engines))
+  (gethash engine jsch/engines))
 
-(create-search-command region     :input 'keyword-from-region
-                                  :engine (lambda () (read-from-minibuffer "Engine? ")))
-(create-search-command minibuffer :input 'keyword-from-minibuffer
-                                  :engine (lambda () (read-from-minibuffer "Engine? ")))
-(create-search-command anywhere   :input 'keyword-from-anywhere
-                                  :engine (lambda () (read-from-minibuffer "Engine? ")))
+(jsch/create-search-command region     :input 'jsch/keyword-from-region
+                                       :engine (lambda () (read-from-minibuffer "Engine? ")))
+(jsch/create-search-command minibuffer :input 'jsch/keyword-from-minibuffer
+                                       :engine (lambda () (read-from-minibuffer "Engine? ")))
+(jsch/create-search-command anywhere   :input 'jsch/keyword-from-anywhere
+                                       :engine (lambda () (read-from-minibuffer "Engine? ")))
 
-(make-engine naver "https://search.naver.com/search.naver?where=nexearch&ie=utf8&query=")
-(make-engine google "https://www.google.co.kr/#q=")
-(make-engine stackoverflow "https://www.google.co.kr/#q=site:stackoverflow.com+")
+(jsch/make-engine naver "https://search.naver.com/search.naver?where=nexearch&ie=utf8&query=")
+(jsch/make-engine google "https://www.google.co.kr/#q=")
+(jsch/make-engine stackoverflow "https://www.google.co.kr/#q=site:stackoverflow.com+")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun keyword-from-minibuffer* (default)
+(defun jsch/keyword-from-minibuffer* (default)
   "Gets keyword from minibuffer.
 
    DEFAULT : default string to use when user inserted nothing to minibuffer."
@@ -81,22 +81,22 @@
               (if default (format "(default %s)" default) "")))
      default))
 
-(defun keyword-from-minibuffer ()
+(defun jsch/keyword-from-minibuffer ()
   "Gets keyword from minibuffer (default = word currently pointing at)"
 
-  (keyword-from-minibuffer* (thing-at-point 'symbol)))
+  (jsch/keyword-from-minibuffer* (thing-at-point 'symbol)))
 
-(defun keyword-from-region ()
+(defun jsch/keyword-from-region ()
   "Gets keyword from selected region"
 
   (let ((s (region-beginning)) (e (region-end)))
     (buffer-substring s e)))
 
-(defun keyword-from-anywhere ()
+(defun jsch/keyword-from-anywhere ()
   "If something is selected, returns it. Otherwise, returns result of keyword-from-minibuffer"
 
   (if (region-active-p)
-    (keyword-from-minibuffer* (keyword-from-region))
-    (keyword-from-minibuffer)))
+    (jsch/keyword-from-minibuffer* (jsch/keyword-from-region))
+    (jsch/keyword-from-minibuffer)))
 
 (provide 'just-search)
