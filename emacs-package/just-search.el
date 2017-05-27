e926ac9095000491242ec403f1da08e6048bfc6b
Cheatinikle <kunwoo3526@gmail.com>
2017-05-27 14:47:36 +0900

    Added documents.

--- emacs-package/just-search.el
+++ emacs-package/just-search.el
@@ -3,16 +3,30 @@
 (require 'just-utils)
 
 (defun search-with (prefix keyword)
-  "Searches keyword with given prefix(search engine)"
-  (browse-url (concat prefix keyword))
-  )
+  "Searches keyword with given prefix.
+
+   PREFIX : Url prefix. (such as \"http://google.co.kr/#q=\")
+   KEYWORD : keyword you want to search.
+
+   ex : (search-with \"http://google.co.kr/#q=\" \"lisp\")"
+
+  (browse-url (concat prefix keyword)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;;;
 
 (defvar engines (make-hash-table :test 'equal) "Engine lists")
 
 (cl-defmacro create-search-command (name &key input engine)
-  "Given function with no argument that somehow gets user input keyword,  creates command with it."
+  "A macro to create a keyword-searching function.
+   NAME : name of the function you want to create.
+   INPUT : function with no argument that returns string.
+   ENGINE : function with no argument that returns string.
+            (this string must be the name of predefined just-search engines)
+
+   ex : (create-search-command test :input (lambda () \"lisp\")
+                                    :engine (lambda () \"google\"))
+        => just-search-test"
+
   `(defun ,(macro-conc 'just-search- name) ()
      (interactive)
      (let ((prefix (get-engine (funcall ,engine))))
@@ -22,14 +36,25 @@
      ))
 
 (defmacro make-engine (name prefix)
-  "Creates the function that searches given keyword with engine. Puts engine to engine lists"
+  "Creates just-search engine. More exactly, creates a function that automatically searches given keyword and puts it to the just-search engine dictionary.
+
+   NAME : name of the engine.
+   PREFIX : Url prefix. (such as \"http://google.co.kr/#q=\")
+
+   ex : (make-engine google \"http://google.co.kr/#q=\")"
+
   `(progn
      (create-search-command ,name :input 'keyword-from-anywhere
-                                 :engine (lambda () ,(symbol-name name)))
+                                  :engine (lambda () ,(symbol-name name)))
      (puthash ,(symbol-name name) ,prefix engines)))
 
 (defun get-engine (engine)
-  "Gets engine frm engine lists"
+  "Gets engine from just-search engine dictionary.
+
+   ENGINE : name of the engine (must be string type)
+
+   ex : (get-engine \"google\") "
+
   (gethash engine engines))
 
 (create-search-command region     :input 'keyword-from-region
@@ -46,6 +71,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun keyword-from-minibuffer* (default)
+  "Gets keyword from minibuffer.
+
+   DEFAULT : default string to use when user inserted nothing to minibuffer."
+
   (if-empty
     (read-from-minibuffer
       (format "Insert keyword %s : "
@@ -53,14 +82,19 @@
      default))
 
 (defun keyword-from-minibuffer ()
+  "Gets keyword from minibuffer (default = word currently pointing at)"
+
   (keyword-from-minibuffer* (thing-at-point 'symbol)))
 
 (defun keyword-from-region ()
   "Gets keyword from selected region"
+
   (let ((s (region-beginning)) (e (region-end)))
     (buffer-substring s e)))
 
 (defun keyword-from-anywhere ()
+  "If something is selected, returns it. Otherwise, returns result of keyword-from-minibuffer"
+
   (if (region-active-p)
     (keyword-from-minibuffer* (keyword-from-region))
     (keyword-from-minibuffer)))
