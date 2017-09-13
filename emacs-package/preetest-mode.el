016d0814ce6c5be3580b8704c642a9689febf520
Cheatinikle <kunwoo3526@gmail.com>
2017-09-13 17:48:55 +0900

    No minor mode, applied with-directory to code etc

--- emacs-package/preetest-mode.el
+++ emacs-package/preetest-mode.el
@@ -1,5 +1,3 @@
-; TODO- add with-dir function in just-utils.
-
 (require 'just-utils)
 
 (defconst PREETEST-PKG-FILE "preetest.pkg")
@@ -14,65 +12,65 @@
 (defvar preetest-current-loc nil)
 (defvar preetest-q-number 0)
 
-(define-minor-mode preetest-mode
-  "Create your own test"
-  :init-value nil
-  (if preetest-mode
-      (progn
-        (unless preetest-current-loc
-          (preetest-open-test))))
-  )
-
 (defun preetest-create-test ()
   (interactive)
-  (let ((location (read-file-name "Create test - Select location λ  ")))
+  (let ((location (read-directory-name "Create test - Select location λ  ")))
     (preetest--create-test location)))
 
 (defun preetest-open-test ()
   (interactive)
-  (let ((location (read-file-name "Open test - Select location λ  ")))
+  (let ((location (read-directory-name "Open test - Select location λ  ")))
     (preetest--open-test location)))
 
 (defun preetest-package? (location)
-  (cd location)
-  (string-equal (get-string-from-file location) PREETEST-MAGIC-TEXT))
+  (with-directory location
+    (string-equal (get-string-from-file PREETEST-PKG-FILE)
+                  PREETEST-MAGIC-TEXT)))
 
 (defun preetest-add-question ()
   (interactive)
-  (cd preetest-current-loc)
-  (preetet--add-element preetest-q-number PREETEST-QUESTION-DIR)
-  (preetet--add-element preetest-q-number PREETEST-ANSWER-DIR)
-  )
+  (preetest--add-question preetest-q-number preetest-current-loc))
 
-; TODO
-(defun preetest--add-element (n location)
-  (dolist (i (number-sequence n (preetest--get-last location)))
-    (rename-file (number-to-string i) (number-to-string (1+ i))))\
-  (create-file (number-to-string n)))
+(defun preetest--add-question (n location)
+  (with-directory location
+    (preetest--add-element n PREETEST-QUESTION-DIR)
+    (preetest--add-element n PREETEST-ANSWER-DIR)))
 
 (defun preetest--create-test (location)
   (mkdir location)
-  (cd location)
-  (with-temp-file PREETEST-PKG-FILE
-    (insert PREETEST-MAGIC-TEXT))
-  (mkdir PREETEST-QUESTION-DIR)
-  (mkdir PREETEST-ANSWER-DIR)
-  
-  (with-temp-file (concat PREETEST-QUESTION-DIR "0")
-    (insert PREETEST-QUESTION-DEFAULT))
-  ;TODO - correct into correct function.
-  (create-file (concat PREETEST-ANSWER-DIR "0"))
+  (with-directory location
+    (with-temp-file PREETEST-PKG-FILE
+      (insert PREETEST-MAGIC-TEXT))
+    (mkdir PREETEST-QUESTION-DIR)
+    (mkdir PREETEST-ANSWER-DIR)
+
+    (with-temp-file (concat PREETEST-QUESTION-DIR "0")
+      (insert PREETEST-QUESTION-DEFAULT))
+    (create-file (concat PREETEST-ANSWER-DIR "0")))
 
   (preetest--open-test location))
 
 (defun preetest--open-test (location)
-  (unless (preetest-package? location) (error "Not a preetest test!"))
-  (setq preetest-current-loc location)
-  
-  (preetest-mode))
-  
-; TODO - handle "." and ".." etc
+  (with-directory location
+    (unless (preetest-package? location) (error "Not a preetest test!"))
+    (setq preetest-current-loc location)
+
+    ;;(preetest-mode))
+))
+
+(defun preetest--add-element (n location)
+  (with-directory location
+    (dolist (i (number-sequence (preetest--get-last location) n -1))
+      (rename-file (number-to-string i) (number-to-string (1+ i))))
+    (create-file (number-to-string n))))
+
+(defun preetest--remove-element (n location)
+  (with-directory location
+    (delete-file (number-to-string n))
+    (dolist (i (number-sequence n (preetest--get-last location)))
+      (rename-file (number-to-string i) (number-to-string (1- i))))))
+
 (defun preetest--get-last (dir)
   (seq-max (-map 'string-to-number (directory-files dir))))
-  
-(provide 'preetest-mode)
+
+(provide 'preetest)
